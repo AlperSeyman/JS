6af58ac1dc8237eb4ee2a467421e09a328982a4d
Alper Seyman <89801493+AlperSeyman@users.noreply.github.com>
2025-12-24 20:53:31 +0100

    added class keyword

--- Objects/index.js
+++ Objects/index.js
@@ -3,17 +3,6 @@
 //           object = {key:value,
 //                     function()}
 
-// this = reference to the object where THIS is used
-//        (the object depends on the immediate context)
-//        person.name = this.name
-
-
-// constructor = special method for defining the properties and methods of objects
-
-
-// class = (ES6 feature) provides a more structured and cleaner way to
-//          work with objects compored to traditional constructor function
-//          ex. static keyword, encapsulation, inheritance
 
 // const person1 = {
 //     firstName: "Spongebob",
@@ -23,6 +12,13 @@
 //     sayHello: ()=>console.log("Hi! I am Spongebob!"),
 //     sayBye: ()=>console.log("Goodbye!")
 // }
+
+
+
+// constructor = special method for defining the properties and methods of objects
+// this = reference to the object where THIS is used
+//        (the object depends on the immediate context)
+//        person.name = this.name
 
 //  Constructors
 // function Person(firstName, lastName, age)  {
@@ -35,7 +31,43 @@
 // }
 
 // const person = new Person("test", "nikole", 40)
-
 // person.sayHello()
+
+
+
+
+// class = (ES6 feature) provides a more structured and cleaner way to
+//          work with objects compored to traditional constructor function
+//          ex. static keyword, encapsulation, inheritance
+
+
+class Product{
+    constructor(name, price){
+        this.name = name;
+        this.price = price;
+
+    }
+    displayProduct(){
+        console.log(`Name: ${this.name}`)
+        console.log(`Price: $${this.price.toFixed(2)}`)
+    }
+    calculateTotal(salesTax){
+        return this.price + (this.price * salesTax)
+    }
+}
+
+const salesTax = 0.05;
+
+
+const product1 = new Product("Shirt", 19.99);
+const product2 = new Product("Pants", 22.50);
+const product3 = new Product("Underwear", 100.00);
+
+product1.hello()
+product1.displayProduct()
+
+const total = product1.calculateTotal(salesTax)
+console.log(`Total price (with tax): $${total.toFixed(2)}`);
+
 
 
