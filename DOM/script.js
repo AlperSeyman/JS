dbeadf85b464f173bf43975914d5e148f2263e88
Alper Seyman <89801493+AlperSeyman@users.noreply.github.com>
2025-12-25 17:58:11 +0100

    started DOM

---
+++ DOM/script.js
@@ -0,0 +1,37 @@
+
+let result;
+
+// 1- document.getElementById("id_name")
+// result = document.getElementById("title");
+// result = document.getElementById("title").id;
+// result = document.getElementById("title").className;
+// result = document.getElementById("title").classList;
+
+// result = document.getElementById("title").style.fontSize="40px";
+// result = document.getElementById("title").style.color="red";
+// result = document.getElementById("title").style.display="none";
+
+
+// 2- document.querySelector() -> id, class, elementName
+
+// result = document.querySelector("#title") // id="title"
+// result = document.querySelector(".app-title") // class="app-title"
+// result = document.querySelector("h1") // elementName = h1
+// result = document.querySelector("input[type='submit']");
+// result = document.querySelector("li:nth-child(2)")
+
+
+// 3- document.getElementsByClassName() -> return list
+
+// result = document.getElementsByClassName("item");
+// result = document.getElementsByClassName("item")[0];
+// result = document.getElementsByClassName("item")[1];
+// result = document.getElementsByClassName("item")[2];
+
+// const items = document.getElementsByClassName("item");
+
+// const group1 = document.getElementById("group1")
+
+// const items = group1.getElementsByTagName("li")
+
+
