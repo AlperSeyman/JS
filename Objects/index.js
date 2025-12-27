9b88c6ccf84785f26841f5a7a69e5966f2c65771
Alper Seyman <89801493+AlperSeyman@users.noreply.github.com>
2025-12-27 16:27:29 +0100

    updated Objects folder

--- Objects/index.js
+++ Objects/index.js
@@ -12,3 +12,65 @@ const person1 = {
     sayHello: ()=>console.log("Hi! I am Spongebob!"),
     sayBye: ()=>console.log("Goodbye!")
 }
+
+
+const fruits = [{name:"apple", color:"red", calories:95},
+                {name:"orange", color:"orange", calories:45},
+                {name:"banana", color:"yellow", calories:105},
+                {name:"coconut", color:"white", calories:159},
+                {name:"pineapple", color:"yellow", calories:37}]
+
+// ---------- forEach() ----------
+
+fruits.forEach((fruit)=>{
+    console.log(fruit)
+})
+
+// ---------- map() ----------
+const fruitsName = fruits.map((fruit)=>{
+    return fruit.name
+})
+const fruitsColors = fruits.map((fruit)=>{
+    return fruit.color
+})
+const fruitsCalories = fruits.map((fruit)=>{
+    return fruit.calories
+})
+
+// ---------- filter() ----------
+const yellowFruits = fruits.filter((fruit)=>{
+    if (fruit.color === "yellow"){
+        return fruit
+    }
+})
+const lowCalFruits = fruits.filter((fruit)=>{
+    if (fruit.calories < 100) {
+        return fruit
+    }
+})
+const highCalFruits = fruits.filter((fruit)=>{
+    if (fruit.calories > 100) {
+        return fruit
+    }
+})
+
+// ---------- reduce() ----------
+const maxCalFruit = fruits.reduce((max, currentFruit)=>{
+    if (currentFruit.calories > max.calories) {
+        return currentFruit
+    }else{
+        return max
+    }
+})
+
+const minCalFruit = fruits.reduce((min, currentFruit)=>{
+    if (currentFruit.calories < min.calories) {
+        return currentFruit
+    }
+    else{
+        return min
+    }
+})
+
+console.log(maxCalFruit)
+console.log(minCalFruit)
